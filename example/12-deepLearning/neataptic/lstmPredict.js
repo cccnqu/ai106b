6b66a2ebfad3f5b8be409f8f7697840d938293a8
ccckmit <ccckmit@gmail.com>
2018-06-13 11:42:44 +0800

    add \n to lstmPredict.js for byte version

--- example/12-deepLearning/neataptic/lstmPredict.js
+++ example/12-deepLearning/neataptic/lstmPredict.js
@@ -3,6 +3,7 @@ var fs = require('fs')
 
 var argv = process.argv
 var maxLen = (argv.length >= 4) ? parseInt(argv[3]) : 100
+LSTM.mode = (process.argv.length >= 5) ? process.argv[4] : 'char'
 var json = fs.readFileSync(process.argv[2])
 LSTM.fromJSON(json)
 var sLines = ['', '\n']
